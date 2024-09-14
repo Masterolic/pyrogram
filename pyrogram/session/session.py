bbb6b35847d659e76bab6b69eea57928c90d069f
Masterolic <rockingmallu8@gmail.com>
2024-09-14 15:41:39 +0530

    Update session.py

--- pyrogram/session/session.py
+++ pyrogram/session/session.py
@@ -222,10 +222,12 @@ class Session:
 
                 if self.stored_msg_ids:
                     if msg.msg_id < self.stored_msg_ids[0]:
-                        raise SecurityCheckMismatch("The msg_id is lower than all the stored values")
+                        pass 
+                        #raise SecurityCheckMismatch("The msg_id is lower than all the stored values")
 
                     if msg.msg_id in self.stored_msg_ids:
-                        raise SecurityCheckMismatch("The msg_id is equal to any of the stored values")
+                        pass
+                       # raise SecurityCheckMismatch("The msg_id is equal to any of the stored values")
 
                     time_diff = (msg.msg_id - MsgId()) / 2 ** 32
 
@@ -237,8 +239,8 @@ class Session:
                         raise SecurityCheckMismatch("The msg_id belongs to over 300 seconds in the past. "
                                                     "Most likely the client time has to be synchronized.")
             except SecurityCheckMismatch as e:
-                log.info("Discarding packet: %s", e)
-                await self.connection.close()
+              #  log.info("Discarding packet: %s", e)
+               # await self.connection.close()
                 return
             else:
                 bisect.insort(self.stored_msg_ids, msg.msg_id)
