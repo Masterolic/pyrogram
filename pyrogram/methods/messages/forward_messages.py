9fcf7e91c722039f8fc0175ec1d7162080de9da5
Masterolic <rockingmallu8@gmail.com>
2024-10-02 22:07:18 +0530

    Update forward_messages.py

--- pyrogram/methods/messages/forward_messages.py
+++ pyrogram/methods/messages/forward_messages.py
@@ -18,7 +18,7 @@
 
 from datetime import datetime
 from typing import Union, List, Iterable
-import time.time
+import time 
 import pyrogram
 from pyrogram import raw, utils
 from pyrogram import types
