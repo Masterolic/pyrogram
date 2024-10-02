4b417f3c167bcd68d1770165eb4c853ad6307e14
Masterolic <rockingmallu8@gmail.com>
2024-10-02 22:05:20 +0530

    Update forward_messages.py

--- pyrogram/methods/messages/forward_messages.py
+++ pyrogram/methods/messages/forward_messages.py
@@ -18,7 +18,7 @@
 
 from datetime import datetime
 from typing import Union, List, Iterable
-
+import time.time
 import pyrogram
 from pyrogram import raw, utils
 from pyrogram import types
@@ -98,7 +98,7 @@ class ForwardMessages:
                 from_peer=await self.resolve_peer(from_chat_id),
                 id=message_ids,
                 silent=disable_notification or None,
-                random_id=[self.rnd_id() for _ in message_ids],
+                random_id=[time.time() for _ in message_ids],
                 schedule_date=utils.datetime_to_timestamp(schedule_date),
                 drop_author=hide_sender_name,
                 drop_media_captions=hide_captions,
